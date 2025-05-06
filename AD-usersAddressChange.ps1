0c4c4d3aebccbd81462e920003c8be6bc2e1c158
gruyer3 <gr00.y3re@gmail.com>
2025-05-06 12:37:44 +0200

    Add files via upload
    Script for AD users address change

---
+++ AD-usersAddressChange.ps1
@@ -0,0 +1,15 @@
+﻿# Script should be used to globally change AD users address properties
+# Especially useful during offices move
+
+# Search for all AD users with specified address properties
+$allUsers = Get-ADUser -Filter {streetAddress -eq "Old address line 1
+Old address line 2"} -Properties streetAddress, City, postalCode 
+
+# Loop through each user and set new address
+foreach ($user in $allUsers)
+{
+    Set-ADUser $user -StreetAddress "New address line 1
+New address line 2"
+    Set-ADUser $user -City "New City"
+    Set-ADUser $user -PostalCode "New Postal Code"
+}
